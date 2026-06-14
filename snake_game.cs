1f324d6a36934a6d27fe42893005f767354a0314
adamharmaniak <harmaniakadam@gmail.com>
2026-06-14 21:54:00 +0200

    Oddelenie samotnej hry od main

--- snake_game.cs
+++ snake_game.cs
@@ -18,23 +18,11 @@ namespace Snake
         {
             
             var settings = new GameSettings(32, 16, 500, 5);
-            
-            int screenwidth = Console.WindowWidth;
-            int screenheight = Console.WindowHeight;
-            Random randomnummer = new Random();
-            int gameover = 0;
-            pixel hoofd = new pixel();
-            hoofd.xpos = screenwidth/2;
-            hoofd.ypos = screenheight/2;
-            hoofd.schermkleur = ConsoleColor.Red;
-            string movement = "RIGHT";
-            List<int> xposlijf = new List<int>();
-            List<int> yposlijf = new List<int>();
-            int berryx = randomnummer.Next(0, screenwidth);
-            int berryy = randomnummer.Next(0, screenheight);
-            DateTime tijd = DateTime.Now;
-            DateTime tijd2 = DateTime.Now;
-            string buttonpressed = "no";
+
+            ConfigureConsole(settings);
+
+            var game = new SnakeGame(settings, new Random());
+
             while (true)
             {
                 Console.Clear();
@@ -148,7 +136,22 @@ namespace Snake
             Console.SetCursorPosition(screenwidth / 5, screenheight / 2 +1);
         }
 
-        internal sealed class GameSettings
+        private static void ConfigureConsole(GameSettings settings)
+        {
+            Console.WindowWidth = settings.Width;
+            Console.WindowHeight = settings.Height;
+            Console.CursorVisible = false;
+        }
+
+        class pixel
+        {
+            public int xpos { get; set; }
+            public int ypos { get; set; }
+            public ConsoleColor schermkleur { get; set; }
+        }
+    }
+
+    internal sealed class GameSettings
         {
             public GameSettings(int width, int height, int frameDelayMilliseconds, int initialScore)
             {
@@ -164,11 +167,120 @@ namespace Snake
             public int InitialScore { get; }
         }
 
-        class pixel
+    internal sealed class SnakeGame
+    {
+        private readonly GameSettings settings;
+        private readonly Random random;
+        private readonly LinkedList<Cell> snake = new LinkedList<Cell>();
+
+        public SnakeGame(GameSettings settings, Random random)
         {
-            public int xpos { get; set; }
-            public int ypos { get; set; }
-            public ConsoleColor schermkleur { get; set; }
+            this.settings = settings;
+            this.random = random;
+
+            Score = settings.InitialScore;
+            Direction = Direction.Right;
+
+            snake.AddFirst(new Cell(settings.Width / 2, settings.Height / 2));
+            Food = CreateFood();
+        }
+
+        public int Score { get; private set; }
+        public bool IsGameOver { get; private set; }
+        public Direction Direction { get; private set; }
+        public Cell Food { get; private set; }
+        public Cell Head => snake.First.Value;
+        public IEnumerable<Cell> SnakeCells => snake;
+
+        public void ChangeDirection(Direction newDirection)
+        {
+            if (!DirectionHelper.AreOpposite(Direction, newDirection))
+            {
+                Direction = newDirection;
+            }
+        }
+
+        public void Update()
+        {
+            if (IsGameOver)
+            {
+                return;
+            }
+
+            Cell nextHead = GetNextHead();
+            bool ateFood = nextHead.Equals(Food);
+
+            snake.AddFirst(nextHead);
+
+            if (ateFood)
+            {
+                Score++;
+                Food = CreateFood();
+            }
+            else
+            {
+                TrimSnake();
+            }
+
+            IsGameOver = HitsWall(nextHead) || HitsItself(nextHead);
+        }
+
+        private Cell GetNextHead()
+        {
+            switch (Direction)
+            {
+                case Direction.Up:
+                    return new Cell(Head.X, Head.Y - 1);
+
+                case Direction.Down:
+                    return new Cell(Head.X, Head.Y + 1);
+
+                case Direction.Left:
+                    return new Cell(Head.X - 1, Head.Y);
+
+                case Direction.Right:
+                    return new Cell(Head.X + 1, Head.Y);
+
+                default:
+                    throw new InvalidOperationException("Unknown direction.");
+            }
+        }
+
+        private void TrimSnake()
+        {
+            while (snake.Count > Score)
+            {
+                snake.RemoveLast();
+            }
+        }
+
+        private bool HitsWall(Cell cell)
+        {
+            return cell.X == 0
+                || cell.Y == 0
+                || cell.X == settings.Width - 1
+                || cell.Y == settings.Height - 1;
+        }
+
+        private bool HitsItself(Cell head)
+        {
+            return snake.Skip(1).Contains(head);
+        }
+
+        private Cell CreateFood()
+        {
+            Cell food;
+
+            do
+            {
+                food = new Cell(
+                    random.Next(1, settings.Width - 1),
+                    random.Next(1, settings.Height - 1)
+                );
+            }
+            while (snake.Contains(food));
+
+            return food;
         }
     }
 }
